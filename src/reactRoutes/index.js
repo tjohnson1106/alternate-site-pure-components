9eb93d761ca90275705f2b74fc9470a04b18ad34
Thomas Johnson <tjohnson1106@gmail.com>
2018-01-16 19:11:13 -0500

    got rid of React-Router will use a NPM package called history

--- src/reactRoutes/index.js
+++ src/reactRoutes/index.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React from 'react';
+
+
+const routes = [
+  { path: '/ Menu', action: () => <Menu /> },
+  { path: '/Footer', action: () => <Footer /> },
+  { path: '/Contact', action: () => <Contact /> }
+];
+
+export default routes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React from "react";
 import { BrowserRouter, Router, Route, Switch } from "react-router-dom";
 
 export default () => {
   <BrowserRouter>
     <Switch>
-      <Route path="/Menu" exact component={Menu} />
+      <Route path="/Menu" exact component={props => <Menu {...props} />} />
       <Route path="/Contact" exact component={Contact} />
       <Route path="/Footer" exact component={Footer} />
     </Switch>
   </BrowserRouter>;
-};
+};*/
